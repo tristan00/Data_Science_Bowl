67c9365417949d22fc9dc503eea0c0aeb12ee6af
TD\tdelforge <tristandelforge1@gmail.com>
2018-02-20 20:11:25 -0600

    training 2 models

--- main.py
+++ main.py
@@ -24,7 +24,8 @@ import h5py
 import pickle
 
 max_images = 1000
-sample_per_image = 50000
+sample_per_image_location_model= 10000
+sample_per_image_edge_model= 60000
 files_loc = 'C:/Users/tdelforge/Documents/Kaggle_datasets/data_science_bowl/'
 
 k_means_image_size = (128, 128)
@@ -71,9 +72,9 @@ def get_cnn():
     model = Sequential()
     #model.add(Conv2D(32, (3, 3), input_shape=(32, 32, 1)))
     model.add(ZeroPadding2D((1,1),input_shape=(32, 32, 2)))
-    model.add(Conv2D(16, (3, 3), activation='elu'))
+    model.add(Conv2D(32, (3, 3), activation='elu'))
     model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(16, (3, 3), activation='elu'))
+    model.add(Conv2D(32, (3, 3), activation='elu'))
     model.add(MaxPooling2D((2,2), strides=(2,2)))
 
     model.add(ZeroPadding2D((1,1)))
@@ -104,7 +105,11 @@ def get_cnn():
 
     model.add(Flatten())
 
-    model.add(Dense(1024))
+    model.add(Dense(4096))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU())
+    model.add(Dropout(0.5))
+    model.add(Dense(2048))
     model.add(BatchNormalization())
     model.add(LeakyReLU())
     model.add(Dropout(0.5))
@@ -115,8 +120,100 @@ def get_cnn():
     model.add(Dense(2))
 
     model.add(Activation('softmax'))
-    sgd = optimizers.SGD(lr=0.01, decay=1e-5, momentum=0.0, nesterov=False)
+    sgd = optimizers.SGD(lr=0.001, decay=1e-8, momentum=0.0, nesterov=False)
     model.compile(loss=keras.losses.binary_crossentropy, optimizer=sgd, metrics=['accuracy'])
+    return model
+
+
+def get_image_array_for_location_detection(image, np_gradient_image, size, x, y, masks):
+    output_dict = dict()
+    sub_image = image[int(x):int(x+size), int(y):int(y+size)]
+    sub_image = np.expand_dims(sub_image, axis = 2)
+    sub_image_gradient = np.expand_dims(np_gradient_image[int(x):int(x+size), int(y):int(y+size)], axis = 2)
+    output_dict['image'] = np.dstack([sub_image, sub_image_gradient])
+
+    output = 0
+    for i in masks:
+        if i[x,y] > 0:
+            output = 1
+
+    output_dict['output'] = output
+    if output_dict['image'].shape != (size, size, 2):
+        print(x,y,output_dict['image'].shape)
+        return None
+    else:
+        return output_dict
+
+
+def get_image_arrays_for_location_detection_training(input_image, size, masks):
+    inputs = []
+    result_dicts = []
+
+    adj_image = np.pad(input_image, size//2, mode='constant')
+    np_gradient_image = gaussian_gradient_magnitude(adj_image, sigma=.4)
+
+    for i in range(input_image.shape[0]):
+        for j in range(input_image.shape[1]):
+            inputs.append((i,j))
+    if len(inputs) > sample_per_image_location_model:
+        inputs = random.sample(inputs, sample_per_image_location_model)
+    for i in inputs:
+        result_dicts.append(
+            get_image_array_for_location_detection(adj_image,  np_gradient_image, size, i[0], i[1], masks))
+    result_dicts = [i for i in result_dicts if i]
+
+    df = pd.DataFrame.from_dict(result_dicts)
+
+    try:
+        positive_matches = df[df['output'] > 0]
+        negative_matches = df[df['output'] == 0]
+        negative_matches = negative_matches.sample(n=positive_matches.shape[0])
+        df = pd.concat([positive_matches, negative_matches], ignore_index=True)
+    except:
+        #TODO: handle case with more positives than negatives
+        pass
+    df = df.sample(frac=1)
+
+    return df
+
+
+def get_dataframes_for_location_training(square_size):
+    gen = generate_input_image_and_masks()
+    dfs = []
+
+    for count, _ in enumerate(range(max_images)):
+        print(count)
+        try:
+            image, image_gm, masks, _ = next(gen)
+            #dfs.append(create_image_features(image, image_gm, masks, square_size))
+            dfs.append(get_image_arrays_for_location_detection_training(image, square_size, masks))
+        except StopIteration:
+            traceback.print_exc()
+            break
+
+    print('images read')
+    df = pd.concat(dfs, ignore_index=True)
+    df = df.sample(frac=1)
+    return df
+
+
+def get_location_detection_model():
+    try:
+        model = load_model(files_loc + 'cnn_location.h5')
+        print(model)
+    except:
+        traceback.print_exc()
+        df = get_dataframes_for_location_training(32)
+        x_train, x_test, y_train, y_test = get_model_inputs(df, x_labels=['image'])
+        #train_models(x_train, x_test, y_train, y_test)
+
+        model = get_cnn()
+
+        y_train = keras.utils.to_categorical(y_train, 2)
+        y_test = keras.utils.to_categorical(y_test, 2)
+
+        model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=4)
+        model.save(files_loc + 'cnn_location.h5')
     return model
 
 
@@ -125,13 +222,13 @@ def get_image_array_for_edge_detection(image, np_gradient_image, size, x, y, mas
     sub_image = image[int(x):int(x+size), int(y):int(y+size)]
     sub_image = np.expand_dims(sub_image, axis = 2)
     sub_image_gradient = np.expand_dims(np_gradient_image[int(x):int(x+size), int(y):int(y+size)], axis = 2)
-
     output_dict['image'] = np.dstack([sub_image, sub_image_gradient])
+
     output = 0
     for i in masks:
-        if i[x,y] > 0:
+        if i[x,y] == 0:
             temp_square = i[x-1:x+2, y-1: y+2]
-            if min(temp_square.shape) > 0 and np.mean(temp_square) < np.max(temp_square):
+            if min(temp_square.shape) > 0 and np.mean(temp_square) > np.max(temp_square):
                 output = 1
             elif min(temp_square.shape) == 0:
                 output = 1
@@ -154,8 +251,8 @@ def get_image_arrays_for_edge_detection_training(input_image, size, masks):
     for i in range(input_image.shape[0]):
         for j in range(input_image.shape[1]):
             inputs.append((i,j))
-    if len(inputs) > sample_per_image:
-        inputs = random.sample(inputs, sample_per_image)
+    if len(inputs) > sample_per_image_edge_model:
+        inputs = random.sample(inputs, sample_per_image_edge_model)
     for i in inputs:
         result_dicts.append(
             get_image_array_for_edge_detection(adj_image,  np_gradient_image, size, i[0], i[1], masks))
@@ -196,6 +293,26 @@ def get_dataframes_for_edge_detection_training(square_size):
     return df
 
 
+def get_edge_detection_model():
+    try:
+        model = load_model(files_loc + 'cnn_edge.h5')
+        print(model)
+    except:
+        traceback.print_exc()
+        df = get_dataframes_for_edge_detection_training(32)
+        x_train, x_test, y_train, y_test = get_model_inputs(df, x_labels=['image'])
+        #train_models(x_train, x_test, y_train, y_test)
+
+        model = get_cnn()
+
+        y_train = keras.utils.to_categorical(y_train, 2)
+        y_test = keras.utils.to_categorical(y_test, 2)
+
+        model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=5)
+        model.save(files_loc + 'cnn_edge.h5')
+    return model
+
+
 def get_model_inputs(df, x_labels=['image'], test_size = 0.01):
     print('testing inputs: {0}'.format(x_labels))
     x, y = [], []
@@ -226,24 +343,6 @@ def get_model_inputs(df, x_labels=['image'], test_size = 0.01):
 
     return x_train, x_test, y_train, y_test
 
-def get_edge_detection_model():
-    try:
-        model = load_model(files_loc + 'cnn3.h5')
-        print(model)
-    except:
-        traceback.print_exc()
-        df = get_dataframes_for_edge_detection_training(32)
-        x_train, x_test, y_train, y_test = get_model_inputs(df, x_labels=['image'])
-        #train_models(x_train, x_test, y_train, y_test)
-
-        model = get_cnn()
-
-        y_train = keras.utils.to_categorical(y_train, 2)
-        y_test = keras.utils.to_categorical(y_test, 2)
-
-        model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=5)
-        model.save(files_loc + 'cnn3.h5')
-    return model
 
 def extract_solutions_from_edges():
     pass
@@ -267,13 +366,19 @@ def get_predictions(model1, sub_image_size):
             np_image = np_image.reshape(pil_image.size[1], pil_image.size[0])
             #np_image = np_image[0:100, 0:150]
             adj_image = np.pad(np_image, sub_image_size // 2, mode='constant')
+            np_gradient_image = gaussian_gradient_magnitude(adj_image, sigma=.4)
 
             sub_image_array = []
 
             for i in range(np_image.shape[0]):
                 for j in range(np_image.shape[1]):
+                    output_dict = dict()
                     sub_image = adj_image[int(i):int(i + sub_image_size), int(j):int(j + sub_image_size)]
-                    sub_image_array.append(np.expand_dims(sub_image, axis=2))
+                    sub_image = np.expand_dims(sub_image, axis=2)
+                    sub_image_gradient = np.expand_dims(np_gradient_image[int(i):int(i + sub_image_size), int(j):int(j + sub_image_size)],
+                                                        axis=2)
+                    sub_image = np.dstack([sub_image, sub_image_gradient])
+                    sub_image_array.append(sub_image)
                     #sub_image = np.expand_dims(sub_image, axis=0)
 
             sub_image_array = np.array(sub_image_array)
@@ -306,7 +411,10 @@ def get_predictions(model1, sub_image_size):
 
 
 def main():
-    model = get_edge_detection_model()
+    model2 = get_location_detection_model()
+    model1 = get_edge_detection_model()
+
+    #get_predictions(model2, 32)
 
 
 if __name__ == '__main__':
