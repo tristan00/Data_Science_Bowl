1c548b86f08d21c481dbb26145aacdd40da2a041
TD\tdelforge <tristandelforge1@gmail.com>
2018-02-25 07:04:57 -0600

    added svc

--- attempt2.py
+++ attempt2.py
@@ -5,6 +5,7 @@ import numpy as np
 import random
 import functools
 import operator
+import keras.utils
 import traceback
 import multiprocessing
 from scipy.ndimage import gaussian_gradient_magnitude
@@ -15,20 +16,27 @@ from keras.layers.convolutional import Conv2D, Conv2DTranspose
 from keras.layers.merge import concatenate
 from keras import backend as K
 from keras.layers import Input
+from sklearn import preprocessing
+from sklearn.svm import SVC
 from keras.layers.core import Layer, Dense, Dropout, Activation, Flatten, Reshape, Permute
 import os
 from keras.layers.convolutional import Convolution2D, MaxPooling2D, UpSampling2D, ZeroPadding2D
+import h5py
+import configparser
+import ast
 
-confidence_threshold = .5
-max_images = 1000
-sample_per_image_location_model= 5000
-sample_per_image_edge_model= 60000
-files_loc = 'C:/Users/tdelforge/Documents/Kaggle_datasets/data_science_bowl/'
+config = configparser.ConfigParser()
+config.read('properties.ini')
+section = 'main_pc'
 
-full_image_read_size = (128, 128)
-min_nuclei_size = 10
+max_images = ast.literal_eval(config.get(section, 'max_images'))
+files_loc = config.get(section, 'file_loc')
+min_nuclei_size = ast.literal_eval(config.get(section, 'min_nuclei_size'))
+confidence_threshold = ast.literal_eval(config.get(section, 'confidence_threshold'))
+full_image_read_size = ast.literal_eval(config.get(section, 'full_image_read_size'))
 
 
+#TODO: figure out gradient class issue
 def mean_iou(y_true, y_pred):
     prec = []
     for t in np.arange(0.5, 1.0, 0.05):
@@ -43,7 +51,7 @@ def mean_iou(y_true, y_pred):
 
 #taken from https://www.kaggle.com/keegil/keras-u-net-starter-lb-0-277
 def get_cnn():
-    inputs = Input((128, 128, 2))
+    inputs = Input((full_image_read_size[0], full_image_read_size[1], 2))
     s = Lambda(lambda x: x / 255)(inputs)
 
     c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(s)
@@ -122,11 +130,11 @@ def generate_input_image_and_masks():
         except OSError:
             continue
 
-
         yield np_image, masks
 
-
-def get_subimages(input_image, gradient, input_mask, max_subimages, transpose = False, rotation = 0):
+#augmentation function
+#TODO: add forms of augmentation with changed lighting or a few randomly sligtly altered pixels
+def get_subimages(input_image, gradient, input_mask, transpose = False, rotation = 0):
     if transpose:
         input_image = np.transpose(input_image)
         input_mask = np.transpose(input_mask)
@@ -135,18 +143,22 @@ def get_subimages(input_image, gradient, input_mask, max_subimages, transpose = 
     input_mask = np.rot90(input_mask, rotation)
     input_gradient = np.rot90(gradient, rotation)
 
+    max_x_subimages  = (input_image.shape[0])//full_image_read_size[0]
+    max_y_subimages = (input_image.shape[1]) // full_image_read_size[1]
+
     output = []
-    for i in range(max_subimages):
-        for j in range(max_subimages):
+    for i in range(max_x_subimages):
+        for j in range(max_y_subimages):
             x1 = i*full_image_read_size[0]
             x2 = (1+i)*full_image_read_size[0]
-            y1 = j*full_image_read_size[0]
-            y2 = (1+j)*full_image_read_size[0]
-
-
-            output.append({'input':np.dstack((np.expand_dims(input_image[x1:x2,y1:y2], axis=2),
+            y1 = j*full_image_read_size[1]
+            y2 = (1+j)*full_image_read_size[1]
+            next_input = {'input':np.dstack((np.expand_dims(input_image[x1:x2,y1:y2], axis=2),
                                              np.expand_dims(input_gradient[x1:x2,y1:y2], axis=2))),
-                           'output':np.expand_dims(input_mask[x1:x2,y1:y2], axis=2)})
+                           'output':np.expand_dims(input_mask[x1:x2,y1:y2], axis=2)}
+
+            output.append(next_input)
+            #print(next_input['input'].shape, next_input['output'].shape)
     return output
 
 
@@ -162,14 +174,14 @@ def get_image_arrays_for_full_location_training(input_image, masks):
     mask_sum = vectorized(mask_sum)
 
     output = []
-    output.extend(get_subimages(input_image, gradient, mask_sum, max_subimages, transpose=False, rotation=0))
-    output.extend(get_subimages(input_image, gradient, mask_sum, max_subimages, transpose=False, rotation=1))
-    output.extend(get_subimages(input_image, gradient, mask_sum, max_subimages, transpose=False, rotation=2))
-    output.extend(get_subimages(input_image, gradient, mask_sum, max_subimages, transpose=False, rotation=3))
-    output.extend(get_subimages(input_image, gradient, mask_sum, max_subimages, transpose=True, rotation=0))
-    output.extend(get_subimages(input_image, gradient, mask_sum, max_subimages, transpose=True, rotation=1))
-    output.extend(get_subimages(input_image, gradient, mask_sum, max_subimages, transpose=True, rotation=2))
-    output.extend(get_subimages(input_image, gradient, mask_sum, max_subimages, transpose=True, rotation=3))
+    output.extend(get_subimages(input_image, gradient, mask_sum, transpose=False, rotation=0))
+    output.extend(get_subimages(input_image, gradient, mask_sum, transpose=False, rotation=1))
+    output.extend(get_subimages(input_image, gradient, mask_sum, transpose=False, rotation=2))
+    output.extend(get_subimages(input_image, gradient, mask_sum, transpose=False, rotation=3))
+    output.extend(get_subimages(input_image, gradient, mask_sum, transpose=True, rotation=0))
+    output.extend(get_subimages(input_image, gradient, mask_sum, transpose=True, rotation=1))
+    output.extend(get_subimages(input_image, gradient, mask_sum, transpose=True, rotation=2))
+    output.extend(get_subimages(input_image, gradient, mask_sum, transpose=True, rotation=3))
 
     return pd.DataFrame(output)
 
@@ -178,36 +190,26 @@ def get_image_arrays_for_full_edge_training(tuple_input):
     input_image, masks = tuple_input
     print(input_image.shape)
 
-    max_subimages = (min(input_image.shape))//min(full_image_read_size)
-
     gradient = gaussian_gradient_magnitude(input_image, sigma=.4)
-
-    # masks = [gaussian_gradient_magnitude(i, sigma=.4) for i in masks]
-    # mask_sum = functools.reduce(operator.add, masks)
 
     mask_sum = input_image.copy()
     mask_sum[:] = 0
 
-    for i in range(input_image.shape[0]):
-        for j in range(input_image.shape[1]):
-            for m in masks:
-                if m[i, j] == 0:
-                    small_mask = m[i - 1:i + 2, j - 1: j + 2]
-                    if min(small_mask.shape) > 0 and np.max(small_mask) > 0:
-                        mask_sum[i,j] = 255
+    for m in masks:
+        mask_sum = np.add(gaussian_gradient_magnitude(m, sigma=.4), mask_sum)
 
     vectorized = np.vectorize(lambda t: 1 if t>0 else 0)
     mask_sum = vectorized(mask_sum)
 
     output = []
-    output.extend(get_subimages(input_image, gradient, mask_sum, max_subimages, transpose=False, rotation=0))
-    output.extend(get_subimages(input_image, gradient, mask_sum, max_subimages, transpose=False, rotation=1))
-    output.extend(get_subimages(input_image, gradient, mask_sum, max_subimages, transpose=False, rotation=2))
-    output.extend(get_subimages(input_image, gradient, mask_sum, max_subimages, transpose=False, rotation=3))
-    output.extend(get_subimages(input_image, gradient, mask_sum, max_subimages, transpose=True, rotation=0))
-    output.extend(get_subimages(input_image, gradient, mask_sum, max_subimages, transpose=True, rotation=1))
-    output.extend(get_subimages(input_image, gradient, mask_sum, max_subimages, transpose=True, rotation=2))
-    output.extend(get_subimages(input_image, gradient, mask_sum, max_subimages, transpose=True, rotation=3))
+    output.extend(get_subimages(input_image, gradient, mask_sum, transpose=False, rotation=0))
+    output.extend(get_subimages(input_image, gradient, mask_sum, transpose=False, rotation=1))
+    output.extend(get_subimages(input_image, gradient, mask_sum, transpose=False, rotation=2))
+    output.extend(get_subimages(input_image, gradient, mask_sum, transpose=False, rotation=3))
+    output.extend(get_subimages(input_image, gradient, mask_sum, transpose=True, rotation=0))
+    output.extend(get_subimages(input_image, gradient, mask_sum, transpose=True, rotation=1))
+    output.extend(get_subimages(input_image, gradient, mask_sum, transpose=True, rotation=2))
+    output.extend(get_subimages(input_image, gradient, mask_sum, transpose=True, rotation=3))
 
     return pd.DataFrame(output)
 
@@ -235,23 +237,22 @@ def get_dataframes_for_training_location():
 
 def get_dataframes_for_training_edge():
     gen = generate_input_image_and_masks()
-    edge_dfs = []
 
     p = multiprocessing.Pool(processes=4)
-
     edge_df = pd.concat(list(p.imap(get_image_arrays_for_full_edge_training, gen, chunksize=1)))
+
     #edge_df = functools.reduce(pd.concat, dfs)
 
     # for count, _ in enumerate(range(max_images)):
     #     print(count)
     #     try:
     #         images, masks = next(gen)
-    #         edge_dfs.append(get_image_arrays_for_full_edge_training(images, masks))
+    #         edge_dfs.append(get_image_arrays_for_full_edge_training((images, masks)))
     #     except StopIteration:
     #         traceback.print_exc()
     #         break
-
-
+    #
+    # edge_df = pd.concat(edge_dfs)
     print('images read')
     edge_df = edge_df.sample(frac=1)
 
@@ -265,7 +266,6 @@ def get_model_inputs(df, x_labels, test_size = 0.05):
     #TODO: vectorize
     for _, i in df.iterrows():
         x.append(np.hstack([i[x_label] for x_label in x_labels]))
-        #x.append(np.hstack([i['image'], i['general_image_stats']]))
         y.append(i['output'])
 
     x = np.array(x)
@@ -295,7 +295,7 @@ def get_loc_model():
 
         x_train, x_test, y_train, y_test = get_model_inputs(df_loc, x_labels=['input'])
         loc_model = get_cnn()
-        loc_model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=4)
+        loc_model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=2)
         loc_model.save(files_loc + 'cnn_full_loc.h5')
     return loc_model
 
@@ -309,7 +309,7 @@ def get_edge_model():
         df_edge = get_dataframes_for_training_edge()
         x_train, x_test, y_train, y_test = get_model_inputs(df_edge, x_labels=['input'])
         edge_model = get_cnn()
-        edge_model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=4)
+        edge_model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=2)
         edge_model.save(files_loc + 'cnn_full_edge.h5')
     return edge_model
 
@@ -343,10 +343,11 @@ def get_outputs_from_flat_array(a):
 
 def to_output_format(label_dict, np_image, image_name):
     output_dicts = []
-    for count, i in enumerate(label_dict):
+    for i in label_dict.keys():
+        print('writing', i)
         image_copy = np_image.copy()
         image_copy.fill(0)
-        for j in i:
+        for j in label_dict[i]:
             image_copy[j[0], j[1]] = 1
         image_copy = np.transpose(image_copy)
         flat_image = image_copy.flatten()
@@ -355,15 +356,16 @@ def to_output_format(label_dict, np_image, image_name):
         output_dict['ImageId'] = image_name
         output_dict['EncodedPixels'] = get_outputs_from_flat_array(flat_image)
         output_dicts.append(output_dict)
-        print('output created:', count, image_name)
+        print('output created:', i, image_name)
     return output_dicts
 
 
 def get_nuclei_from_predictions(locations, image_id):
     location_set = set(locations)
     prediction_n_locations = location_set
-    nuclei_predictions = []
+    nuclei_predictions = dict()
 
+    counter = 0
     while len(prediction_n_locations) > 0:
         starting_location = prediction_n_locations.pop()
         prediction_n_locations.add(starting_location)
@@ -394,7 +396,10 @@ def get_nuclei_from_predictions(locations, image_id):
                 break
         for i in temp_neucli_locations:
             prediction_n_locations.remove(i)
-        nuclei_predictions.append(temp_neucli_locations)
+        if len(temp_neucli_locations) >= min_nuclei_size or len(nuclei_predictions.keys()) == 0:
+            nuclei_predictions[counter] = temp_neucli_locations
+            counter += 1
+
         print('clusters found:', len(nuclei_predictions), ' pixels_left:', len(prediction_n_locations),image_id)
     return nuclei_predictions
 
@@ -409,23 +414,67 @@ def prediction_image_to_location_list(prediction_image):
 
 
 def get_outputs(input_dict):
-    output, np_image, image_id = input_dict['output'], input_dict['np_image'], input_dict['image_id']
+    output, np_image, image_id = input_dict['output_n'], input_dict['np_image'], input_dict['image_id']
     locations = prediction_image_to_location_list(output)
     clusters = get_nuclei_from_predictions(locations, image_id)
     return to_output_format(clusters, np_image, image_id)
 
 
-def predict_subimages(input_image, gradient, max_subimages, transpose, rotation, loc_model, edge_model):
+def train_cluster_model(clusters, e_locations):
+    x = []
+    y = []
+    print('classifying {0} unclustered pixels'.format(len(e_locations)))
+
+    for i in clusters.keys():
+        for j in clusters[i]:
+            x.append(np.array([j[0], j[1]]))
+            y.append(np.array([i]))
+    x = np.array(x)
+    y = np.array(y)
+
+    pred_x = []
+    for i in e_locations:
+        pred_x.append(np.array([i[0], i[1]]))
+    pred_x = np.array(pred_x)
+
+    clf = SVC()
+    clf.fit(x, y)
+    predictions = clf.predict(pred_x)
+    for i, j in zip(e_locations, predictions):
+        clusters[j].add(i)
+
+    return clusters
+
+#expirementing with classifying edge pixels
+def get_outputs2(input_dict):
+    output, edges, np_image, image_id = input_dict['output_n'], input_dict['edges'], input_dict['np_image'], input_dict['image_id']
+
+    not_edge_f = np.vectorize(lambda t: 0 if t > 0 else 1)
+    not_edge = not_edge_f(edges)
+
+    nuclei_not_edge = np.multiply(output, not_edge)
+    n_locations = prediction_image_to_location_list(nuclei_not_edge)
+    e_locations = prediction_image_to_location_list(edges)
+    clusters = get_nuclei_from_predictions(n_locations, image_id)
+    clusters = train_cluster_model(clusters, e_locations)
+
+    return to_output_format(clusters, np_image, image_id)
+
+
+def predict_subimages(input_image, gradient, transpose, rotation, model):
     if transpose:
         input_image = np.transpose(input_image)
         gradient = np.transpose(gradient)
     input_image = np.rot90(input_image, rotation)
     input_gradient = np.rot90(gradient, rotation)
 
+    max_x_subimages  = (input_image.shape[0])//full_image_read_size[0]
+    max_y_subimages = (input_image.shape[1]) // full_image_read_size[1]
+
     output = []
-    for i in range(max_subimages):
+    for i in range(max_x_subimages):
         predictions = []
-        for j in range(max_subimages):
+        for j in range(max_y_subimages):
             x1 = i * full_image_read_size[0]
             x2 = (1 + i) * full_image_read_size[0]
             y1 = j * full_image_read_size[0]
@@ -434,7 +483,7 @@ def predict_subimages(input_image, gradient, max_subimages, transpose, rotation,
             temp_gradient = input_gradient[x1:x2,y1:y2]
             model_input = np.dstack([np.expand_dims(temp_image, axis=2), np.expand_dims(temp_gradient, axis=2)])
             model_input = np.expand_dims(model_input, axis = 0)
-            prediction = loc_model.predict(model_input)
+            prediction = model.predict(model_input)
             prediction = np.squeeze(prediction)
             predictions.append(prediction)
         output.append(np.concatenate(predictions, 1))
@@ -445,33 +494,41 @@ def predict_subimages(input_image, gradient, max_subimages, transpose, rotation,
     output = pad
 
     rotated_output = np.rot90(output, 4-rotation)
-
     return rotated_output
 
 
 def predict_image(loc_model, edge_model, np_image, image_id):
     image_gradient = gaussian_gradient_magnitude(np_image, sigma=.4)
-    max_subimages = (min(np_image.shape)) // min(full_image_read_size)
-
     results = []
-    results.append(predict_subimages(np_image, image_gradient, max_subimages, False, 0, loc_model, edge_model))
-    results.append(predict_subimages(np_image, image_gradient, max_subimages, False, 1, loc_model, edge_model))
-    results.append(predict_subimages(np_image, image_gradient, max_subimages, False, 2, loc_model, edge_model))
-    results.append(predict_subimages(np_image, image_gradient, max_subimages, False, 3, loc_model, edge_model))
+    results.append(predict_subimages(np_image, image_gradient, False, 0, loc_model))
+    results.append(predict_subimages(np_image, image_gradient, False, 1, loc_model))
+    results.append(predict_subimages(np_image, image_gradient, False, 2, loc_model))
+    results.append(predict_subimages(np_image, image_gradient, False, 3, loc_model))
     result_array = np.dstack(results)
 
-    mean = np.nanmean(result_array, 2)
-    print(mean.shape)
+    result_mean = np.nanmean(result_array, 2)
+    print(result_mean.shape)
 
-    mean_bool = mean > confidence_threshold
-    prediction_locations = mean_bool.astype(int)
-    input_dict = {'output':prediction_locations, 'image_id':image_id, 'np_image':np_image}
+    prediction_f = np.vectorize(lambda t: 1 if t > confidence_threshold else 0)
+    nuclie_predictions = prediction_f(result_mean)
+
+    edges = []
+    edges.append(predict_subimages(np_image, image_gradient, False, 0, edge_model))
+    edges.append(predict_subimages(np_image, image_gradient, False, 1, edge_model))
+    edges.append(predict_subimages(np_image, image_gradient, False, 2, edge_model))
+    edges.append(predict_subimages(np_image, image_gradient, False, 3, edge_model))
+    edge_array = np.dstack(edges)
+
+    edge_mean = np.nanmean(edge_array, 2)
+    print(edge_mean.shape)
+
+    edge_predictions = prediction_f(edge_mean)
+
+    input_dict = {'output_n':nuclie_predictions, 'edges':edge_predictions, 'image_id':image_id, 'np_image':np_image}
 
     output_dicts = []
-    output_dicts.extend(get_outputs(input_dict))
+    output_dicts.extend(get_outputs2(input_dict))
     return output_dicts
-
-
 
 
 def run_predictions(loc_model, edge_model):
@@ -492,10 +549,12 @@ def run_predictions(loc_model, edge_model):
     df.to_csv('output.csv', index = False)
 
 
-
 def main():
     edge_model = get_edge_model()
+    print('edge model loaded')
     loc_model = get_loc_model()
+    print('loc model loaded')
+
     run_predictions(loc_model, edge_model)
 
 
